4d5ea02a27ab2d759226f1444f6143276047f2fb
Remik <zyduremik@gmail.com>
2024-03-24 09:00:48 +0100

    dziala

--- Program.cs
+++ Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public interface IShape
+{
+    float GetArea();
+}
+
+
+public class Square
+{
+    private float sideLength;
+
+    public Square(float sideLength)
+    {
+        this.sideLength = sideLength;
+    }
+
+    public float GetArea()
+    {
+        return sideLength * sideLength;
+    }
+}
+
+public class SquareAdapter : IShape
+{
+    private Square square;
+
+    public SquareAdapter(Square square)
+    {
+        this.square = square;
+    }
+
+    public float GetArea()
+    {
+        return square.GetArea();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Square square = new Square(5);
+        SquareAdapter adapter = new SquareAdapter(square);
+
+        Console.WriteLine("Pole kwadratu (Square): " + square.GetArea());
+        Console.WriteLine("Pole kwadratu (SquareAdapter): " + adapter.GetArea());
+
+
+        Console.WriteLine("Pole kwadratu (Funkcja oczekująca na GetArea): " + FunctionExpectGetArea(adapter));
+        Console.WriteLine("Pole kwadratu (Funkcja oczekująca na GetArea z adapterem): " + FunctionExpectGetArea(adapter));
+    }
+
+    static float FunctionExpectGetArea(IShape obj)
+    {
+        return obj.GetArea();
+    }
+}
